e3ed7056a20909d2e4383f7d7d914b9f54979555
Astro <gabrielagomezmolina@gmail.com>
2024-03-12 21:29:25 -0500

    Cambio Final

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ if hist_button:
     st.write('Histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+
 dis_button = st.button('Construir diagrama de dispersion') 
-if hist_button:
+if dis_button:
     st.write('Diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
