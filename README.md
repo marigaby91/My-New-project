06a07d1a1ddee163cfd96f112e90c7b9238bfa37
Astro <gabrielagomezmolina@gmail.com>
2024-03-12 21:14:43 -0500

    Cambios finales

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# My-New-project
+Sprint 5 : Aplicacion WEB vehiculos
+La aplicacion permite realizar de manera grafica un analisis de las tendencias de ventas de los vehiculos en estados unidos,  para la toma de decisiones en cuanto a la publicidad de los mismo, basado en las tendencias identificadas en los datos. 
--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-st.header('Lanzar una moneda')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
+car_data = pd.read_csv('vehicles_us.csv') 
+hist_button = st.button('Construir histograma') 
+if hist_button:
+    st.write('Histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+dis_button = st.button('Construir diagrama de dispersion') 
+if hist_button:
+    st.write('Diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
