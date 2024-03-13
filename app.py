0ce08b6ee60a42dc9c735dedf07205756358cf53
Astro <gabrielagomezmolina@gmail.com>
2024-03-12 20:16:37 -0500

    actualizacion Jupyter

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+
 st.header('Lanzar una moneda')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
